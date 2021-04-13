d2caf5b0e695f4aa61ba521f240487d34327de26
Milan Mody <milanm1120@gmail.com>
2021-04-13 23:53:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    student_array.collect {|student| self.new(student)}
+    student_array.collect {|student| new(student)}
   end
 
   def add_student_attributes(attributes_hash)
