846e8c4d2aefafa031c193d37edfe1875e2cc69f
Milan Mody <milanm1120@gmail.com>
2021-04-13 23:08:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,25 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+
+    students = []
+
+    # page.css("div.roster-cards-container").each do |card|
+      page.css("div.student-card").each do |student|     #"div.student-card" obtained from website
+
+        students << {
+          :name => student.css('.student-name').text,
+          :location => student.css('.student-location').text,
+          :profile_url => student.css("a").attr('href').value
+        }
+      end
+    students
   end
 
+
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
